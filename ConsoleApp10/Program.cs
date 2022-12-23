e5fada42c743e680223854512284e23811b53011
USER <USER@DESKTOP-TRQG01D>
2022-12-23 10:42:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.IO;
+StreamWriter f = new StreamWriter("D://Array.txt", false);
+f.WriteLine(Convert.ToChar(65));
+f.WriteLine(Convert.ToChar(66));
+f.WriteLine(Convert.ToChar(67));
+f.WriteLine(Convert.ToChar(68));
+f.WriteLine(Convert.ToChar(69));
+f.Close();
